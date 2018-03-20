83931d466b6f8ab5aaf53060c0082d257d25657f
Jonathan Wood <jonathanshawwood@gmail.com>
2018-03-20 00:06:46 -0400

    License header.

--- api/src/main/java/com/realitylattice/api/ApiService.java
+++ api/src/main/java/com/realitylattice/api/ApiService.java
@@ -1,7 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2018 Jonathan Shaw Wood.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.realitylattice.api;
 
@@ -10,7 +20,6 @@ import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.realitylattice.module.ApiModule;
 import lombok.extern.java.Log;
-import spark.Route;
 import static spark.Spark.*;
 
 /**
@@ -65,6 +74,11 @@ public class ApiService {
                 get("", tileApi.list);
                 post("", tileApi.add);
                 delete("", tileApi.delete);
+                path("/:id/exits", () -> {
+                    get("", tileApi.listExits);
+//                    post("", tileApi.addExit);
+//                    delete("", tileApi.delete);
+                });
             });
 
         });
--- api/src/main/java/com/realitylattice/api/TileApi.java
+++ api/src/main/java/com/realitylattice/api/TileApi.java
@@ -1,13 +1,24 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2018 Jonathan Shaw Wood.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.realitylattice.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
 import com.realitylattice.model.Tile;
+import org.bson.types.ObjectId;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.query.Query;
 import spark.Request;
@@ -47,6 +58,22 @@ public class TileApi {
         @Override
         public Object handle(Request request, Response response) throws Exception {
             return datastore.delete(mapper.readValue(request.body(), Tile.class));
+        }
+    };
+
+    public Route listExits = new Route() {
+        @Override
+        public Object handle(Request request, Response response) throws Exception {
+            final Query<Tile> query = datastore.createQuery(Tile.class).filter("id", new ObjectId(request.params(":id"))).project("id", true).project("exits", true);
+            return query.asList();
+        }
+    };
+
+    public Route addExit = new Route() {
+        @Override
+        public Object handle(Request request, Response response) throws Exception {
+            final Query<Tile> query = datastore.createQuery(Tile.class).filter("id", new ObjectId(request.params(":id"))).project("id", true).project("exits", true);
+            return query.asList();
         }
     };
 
